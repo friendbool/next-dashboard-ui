c796684514c7f8c864267a3c44b237f1f9f7e3cd
friendbool <jonata.ornelli@hotmail.com>
2024-08-31 15:36:58 +0200

    menu di navigazione

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -123,15 +124,17 @@ const Menu = () => {
       {menuItems.map(m => (
         <div key={m.title} className="flex flex-col gap-2">
           <span className="hidden lg:block text-gray-400 font-light my-4">{m.title}</span>
-          {m.items.map(item => (
-            <Link 
-              key={item.label} 
-              href={item.href} 
-              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
-              <Image src={item.icon} alt="" width={20} height={20} />
-              <span className="hidden lg:block">{item.label}</span>
-            </Link>
-          ))}
+          {m.items.map(item => {
+            if (item.visible.includes(role)) {
+              return <Link
+                key={item.label}
+                href={item.href}
+                className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight">
+                <Image src={item.icon} alt="" width={20} height={20} />
+                <span className="hidden lg:block">{item.label}</span>
+              </Link>
+            }
+          })}
         </div>
       ))}
     </div>
