236f5a064350e78aa48015c193497a01f4376cc3
friendbool <jonata.ornelli@hotmail.com>
2024-09-01 18:48:43 +0200

    student teacher, begin parant page

---
+++ src/components/BigCalendar.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
+import moment from 'moment'
+import { calendarEvents } from '@/lib/data'
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import { useState } from 'react'
+
+const localizer = momentLocalizer(moment)
+
+const BigCalendar = () => {
+
+    const [view, setView] = useState<View>(Views.WORK_WEEK)
+    const [date, setDate] = useState(new Date())
+
+    const handleOnChangeView = (selectedView: View) => {
+        setView(selectedView)
+    }
+
+    const handleOnNavigate = (date: Date) => {
+        setDate(date)
+    }
+
+    return (
+        <Calendar
+            localizer={localizer}
+            events={calendarEvents}
+            date={new Date(2024, 7, 26)}
+            startAccessor="start"
+            endAccessor="end"
+            views={["work_week", "day"]}
+            view={view}
+            style={{ height: "98%" }}
+            onView={handleOnChangeView}
+            onNavigate={handleOnNavigate}
+            min={new Date(2025, 1, 1, 8, 0, 0)}
+            max={new Date(2025, 1, 1, 17, 0, 0)}
+        />
+    )
+}
+
+export default BigCalendar
