4230a1d23892f800943c3aa2c77e6bdd03b68c98
friendbool <jonata.ornelli@hotmail.com>
2024-10-13 16:52:11 +0200

    clerk auth and route guard

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { routeAccessMap } from "./lib/settings";
+import { NextResponse } from "next/server";
+
+const matchers = Object.keys(routeAccessMap).map(route => ({
+    matcher: createRouteMatcher([route]),
+    allowedRoles: routeAccessMap[route]
+}))
+
+const isProtectedRoute = createRouteMatcher(['/admin', '/teacher'])
+
+export default clerkMiddleware((auth, req) => {
+    // if (isProtectedRoute(req)) auth().protect()
+    const { sessionClaims } = auth()
+    const role = (sessionClaims?.metadata as { role?: string })?.role
+    console.log("role:", role)
+    for (const { matcher, allowedRoles } of matchers) {
+        if (matcher(req) && !allowedRoles.includes(role!)) {
+            console.log("req", req)
+            return NextResponse.redirect(new URL(`/${role}`, req.url))
+        }
+    }
+})
+
+export const config = {
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+    ],
+};
